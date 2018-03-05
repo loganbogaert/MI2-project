0385d16b326b48126d5bb68e45fa1672e2317ad7
loganbogaert <32387016+loganbogaert@users.noreply.github.com>
2018-03-05 19:24:49 +0100

    Add files via upload

--- Project MI2/scripting_start_menu.js
+++ Project MI2/scripting_start_menu.js
@@ -32,7 +32,7 @@ function hideDiv()
 function loadDam()
 {
     // bool op true zetten
-    var bool = true;
+    var bool = false;
     // locale var 
     var rij = 8;
     // locale var 
@@ -53,12 +53,48 @@ function loadDam()
             bool = !bool;
             // clear classe toevoegen
             if(b==0 && i !=0) {nodes+="clear";}
-            // einde van div afmaken
-            nodes+= "'></div>";
-        }
+            // div opstellen
+            nodes+= "'>";
+            // als rij tussen 0 en 3 zit
+            if(i>=0 && i<3)
+            {
+                // rij 0 en 2
+                if((i==0 && b!=0 || i==2 && b!=0 ) && b % 2 != 0 ) 
+                {
+                    // div opstellen
+                    nodes+= "<div class='dameNoir'></div>";
+                }
+                // rij 1
+                if(i==1 && b!=7 && b % 2 == 0)
+                {
+                    // div opstellen
+                    nodes+= "<div class='dameNoir'></div>";
+                }  
+            }
+            // als rij tussen 5 en 8 zit
+            if(i>=5 && i<8)
+            {
+               // rij 5 en 7
+               if((i==5 && b!=7 || i==7 &&  b!=7) && b % 2 ==0)
+               {
+                   // div opstellen
+                   nodes+= "<div class='dameBlanche'></div>";
+               }
+               // rij 6 
+               if(i==6 && b!=0 && b % 2 != 0 )
+               {
+                   // div opstellen
+                   nodes+= "<div class='dameBlanche'></div>";
+               }
+            }
+            // div opstellen
+            nodes+="</div>";
+         }
         // bool wisselen als het een even getal is, anders klopt het dambord niet
         if(b % 2 == 0) {bool = !bool;}
+       
     }
-    // op scherm zetten
+    // op schermen
     document.getElementById("dam").innerHTML = nodes;
 }
+    
