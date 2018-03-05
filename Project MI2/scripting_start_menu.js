a4823d78f1929b264392b1369fbebbf707e5213e
loganbogaert <32387016+loganbogaert@users.noreply.github.com>
2018-03-05 13:47:04 +0100

    Update : vanaf nu is het dambord aanwezig op het scherm

--- Project MI2/scripting_start_menu.js
+++ Project MI2/scripting_start_menu.js
@@ -8,8 +8,9 @@ function hireDiv()
     // call jquery method
     $(".startSpelMenu").addClass("rotate");
     // call jquery method
-    $(".divHide").addClass("show");
-    
+    $(".divHide").addClass("show"); 
+    // load dam
+    loadDam();
 }
 //********************<function>********************
 function hideDiv()
@@ -23,5 +24,41 @@ function hideDiv()
     // call jquery method
     $(".startSpelMenu").animate({top: "0%"},800);
     // call jquery method
-    $(".startSpelMenu").addClass("rotatetwo");   
+    $(".startSpelMenu").addClass("rotatetwo");  
+    // call jquery method
+    $(".damBord").fadeTo(800, 1); 
+}
+//********************<function>********************
+function loadDam()
+{
+    // bool op true zetten
+    var bool = true;
+    // locale var 
+    var rij = 8;
+    // locale var 
+    var kolom = 8;
+    // locale var
+    var nodes = "";
+    // for lus voor de rijen te tekenen
+    for(i=0;i<rij;i++)
+    {
+        // for lus voor de kollomen te tekenen
+        for(b=0;b<kolom;b++)
+        {
+            // div opstellen
+            nodes+= "<div class='";
+            // kijken of het zwart of wit moet zijn
+            if(bool){nodes+="blackCase ";} else {nodes+="whiteCase ";}
+            // bool veranderen
+            bool = !bool;
+            // clear classe toevoegen
+            if(b==0 && i !=0) {nodes+="clear";}
+            // einde van div afmaken
+            nodes+= "'></div>";
+        }
+        // bool wisselen als het een even getal is, anders klopt het dambord niet
+        if(b % 2 == 0) {bool = !bool;}
+    }
+    // op scherm zetten
+    document.getElementById("dam").innerHTML = nodes;
 }
